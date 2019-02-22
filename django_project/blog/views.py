913081bacbb82c73a0d7bf86409277f8729cef7d
Aniket Jha <aniketkumarj@gmail.com>
2019-02-22 23:51:31 +0530

    Update views.py
    Creating database

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,29 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Aniket',
-        'title': 'Blog post1',
-        'content': 'xyz',
-        'date_posted': 'February 18, 2019'
-
-    },
-    {
-        'author': 'Aniket',
-        'title': 'Blog post2',
-        'content': 'xyz',
-        'date_posted': 'February 18, 2019'
-
-    }
-]
-
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
